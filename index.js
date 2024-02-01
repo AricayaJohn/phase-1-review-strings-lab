49f1e8932679381fa5e0f7d8ed3843f869894b26
John Aricaya <jaricaya@gmail.com>
2024-01-31 22:24:26 -0800

    completed string concatinate and interpolate lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+//ln3 is add variable
+const currentUser = 'Spiderman';
+console.log(currentUser);
+
+//ln7 concatinate string and interpolate a variable 
+const welcomeMessage = `Welcome to Flatbook, ' + ${currentUser}!` ;
+console.log(welcomeMessage);
+
+//ln11 is to change the whole varable message using Uppercase function
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+//ln16 creating a variable assigned for a short greet
+//ln16 writing the short greet with only first letter of user's name and adding a !
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+console.log(shortGreeting);
+
